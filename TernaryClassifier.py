2583dbf39cd674f333b8462c06bf7331378a11a7
KenjiR1 <126754912+KenjiR1@users.noreply.github.com>
2025-04-16 15:30:53 -0600

    Add files via upload

---
+++ TernaryClassifier.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Apr 16 13:40:18 2025
+
+@author: kenji
+"""
+# Code for creating model object
+# input_dim = number of features (dependent variables)
+# Contains 3 neural layers, 2 activation functions
+
+import torch.nn as nn
+
+class TernaryClassifier(nn.Module):
+    def __init__(self, input_dim):
+        super().__init__()
+        self.net = nn.Sequential(
+            nn.Linear(input_dim, 64),
+            nn.ReLU(),
+            nn.Linear(64,32),
+            nn.ReLU(),
+            nn.Linear(32,3)
+            )
+    
+    def forward(self, x):
+        return self.net(x)
+
+
+
+    
